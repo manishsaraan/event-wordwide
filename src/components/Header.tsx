37bb372171fbc54cd3015db1eb1acb4cc4e65571
manishsaraan <manish021js@gmail.com>
2024-06-28 20:37:23 +0530

    add active link in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import { clsx } from "clsx";
+import { motion } from "framer-motion";
 
 const routes = [
   {
@@ -13,14 +18,31 @@ const routes = [
   },
 ];
 export default function Header() {
+  const activePathName = usePathname();
+
   return (
     <header className="flex sm:px-9 px-3 items-center border-b border-white/10 h-14 justify-between">
       <Logo />
-      <nav>
-        <ul className="flex text-sm gap-x-6">
+      <nav className="h-full">
+        <ul className="flex h-full text-sm gap-x-6">
           {routes.map((link) => (
-            <li className="text-white/10 hover:text-white" key={link.path}>
+            <li
+              className={clsx(
+                "hover:text-white flex items-center relative transition",
+                {
+                  "text-white": activePathName === link.path,
+                  "text-white/50": activePathName !== link.path,
+                }
+              )}
+              key={link.path}
+            >
               <Link href={link.path}>{link.name}</Link>
+              {activePathName === link.path && (
+                <motion.div
+                  layoutId="header-active-link"
+                  className="bg-accent h-1 w-full absolute bottom-0"
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
