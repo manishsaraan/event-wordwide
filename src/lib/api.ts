7f1b35ce266d4530ec168e224577c90d0acf0b35
manishsaraan <manish021js@gmail.com>
2024-07-05 22:56:55 +0530

    use unstable_cache to cache method ouptpus

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,47 +1,53 @@
 import { EventoEvent } from "@prisma/client";
 import { capitalize } from "./util";
 import prisma from "./db";
-export async function getEvents(
-  city: string,
-  page = 1
-): Promise<{
-  events: EventoEvent[];
-  totalCount: number;
-}> {
-  const skip = (page - 1) * 6;
-  console.log(city, "city", capitalize(city));
-  let totalEvent = 0;
+import { unstable_cache } from "next/cache";
 
-  if (city !== "all") {
-    totalEvent = await prisma.eventoEvent.count({
+export const getEvents = unstable_cache(
+  async (
+    city: string,
+    page = 1
+  ): Promise<{
+    events: EventoEvent[];
+    totalCount: number;
+  }> => {
+    const skip = (page - 1) * 6;
+    console.log(city, "city", capitalize(city));
+    let totalEvent = 0;
+
+    if (city !== "all") {
+      totalEvent = await prisma.eventoEvent.count({
+        where: {
+          city: capitalize(city),
+        },
+      });
+    } else {
+      totalEvent = await prisma.eventoEvent.count();
+    }
+
+    const events = await prisma.eventoEvent.findMany({
       where: {
-        city: capitalize(city),
+        city: city === "all" ? undefined : capitalize(city),
+      },
+      orderBy: {
+        date: "asc",
+      },
+      take: 6,
+      skip,
+    });
+
+    return { events, totalCount: totalEvent };
+  }
+);
+
+export const getEvent = unstable_cache(
+  async (slug: string): Promise<EventoEvent | null> => {
+    const event = await prisma.eventoEvent.findUnique({
+      where: {
+        slug,
       },
     });
-  } else {
-    totalEvent = await prisma.eventoEvent.count();
+
+    return event;
   }
-
-  const events = await prisma.eventoEvent.findMany({
-    where: {
-      city: city === "all" ? undefined : capitalize(city),
-    },
-    orderBy: {
-      date: "asc",
-    },
-    take: 6,
-    skip,
-  });
-
-  return { events, totalCount: totalEvent };
-}
-
-export async function getEvent(slug: string): Promise<EventoEvent | null> {
-  const event = await prisma.eventoEvent.findUnique({
-    where: {
-      slug,
-    },
-  });
-
-  return event;
-}
+);
